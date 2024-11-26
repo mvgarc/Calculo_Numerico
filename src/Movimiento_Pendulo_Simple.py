c73f5d8cb03cf1d5c2bad08a4d9d0c709748202f
María García <mariav2601@gmail.com>
2024-11-26 16:49:41 -0400

    se desarrolla la funcion de metodo de taylor de segundo orden

--- src/Movimiento_Pendulo_Simple.py
+++ src/Movimiento_Pendulo_Simple.py
@@ -16,3 +16,29 @@ def domega_dt(theta):
 
 def d2theta_dt2(theta):
     return -(g / L) * dtheta_dt(theta)
+
+def taylor_second_order_pendulum(theta0, omega0, h, t_max):
+    t_values = np.arange(0, t_max + h, h)
+    theta_values = [theta0]
+    omega_values = [omega0]
+
+    for i in range(1, len(t_values)):
+        theta_prev = theta_values[-1]
+        omega_prev = omega_values[-1]
+
+        # Calcular siguiente valor de theta y omega
+        theta_next = (
+            theta_prev 
+            + h * dtheta_dt(omega_prev) 
+            + (h**2 / 2) * domega_dt(theta_prev)
+        )
+        omega_next = (
+            omega_prev 
+            + h * domega_dt(theta_prev) 
+            + (h**2 / 2) * d2theta_dt2(theta_prev)
+        )
+
+        theta_values.append(theta_next)
+        omega_values.append(omega_next)
+    
+    return t_values, theta_values, omega_values
