56c264a88df88e308bbd5cd3e083558552412f07
María García <mariav2601@gmail.com>
2024-11-26 16:48:19 -0400

    inicializo las variables del parametros del problema

--- src/Movimiento_Pendulo_Simple.py
+++ src/Movimiento_Pendulo_Simple.py
@@ -1,2 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+g = 9.8  # Aceleración gravitacional (m/s^2)
+L = 2.0  # Longitud del péndulo (m)
+theta0 = 0.2  # Ángulo inicial (rad)
+omega0 = 0.0  # Velocidad angular inicial (rad/s)
+h = 0.1  # Tamaño del paso (s)
+t_max = 10.0  # Tiempo máximo (s)
