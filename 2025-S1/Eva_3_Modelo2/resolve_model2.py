f4aabd1e2685150bc0582e975882ff31c4006638
María García <mariav2601@gmail.com>
2025-04-15 11:33:04 -0400

    Agregar método lagrange_basis para calcular el polinomio base de Lagrange en un punto dado

--- 2025-S1/Eva_3_Modelo2/resolve_model2.py
+++ 2025-S1/Eva_3_Modelo2/resolve_model2.py
@@ -4,3 +4,16 @@ class LagrangeInterpolation:
     def __init__(self, puntos):
         """Inicializa la clase con los puntos dados."""
         self.puntos = puntos
+
+    def lagrange_basis(self, x, i):
+        """Calcula el i-ésimo polinomio base de Lagrange en el punto x."""
+        xi, _ = self.puntos[i]
+        numerador = 1
+        denominador = 1
+
+        for j, (xj, _) in enumerate(self.puntos):
+            if i != j:
+                numerador *= (x - xj)
+                denominador *= (xi - xj)
+
+        return numerador / denominador
