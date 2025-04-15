7968dc8aebf7483e85a2094de9c3658eed35ee4a
María García <mariav2601@gmail.com>
2025-04-15 11:32:40 -0400

    Add LagrangeInterpolation class for polynomial interpolation
    - Implemented the LagrangeInterpolation class to facilitate polynomial interpolation using given points.
    - Initialized the class with a list of points for further processing.

---
+++ 2025-S1/Eva_3_Modelo2/resolve_model2.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+class LagrangeInterpolation:
+    def __init__(self, puntos):
+        """Inicializa la clase con los puntos dados."""
+        self.puntos = puntos
