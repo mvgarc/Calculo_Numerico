a1f518999aa12a277c37570cd23d0e8bd8c8643b
María García <mariav2601@gmail.com>
2025-04-23 16:48:58 -0400

    Agregar clase ReglaTrapecio para implementar el método del trapecio en la integración numérica

---
+++ 2025-S1/Evaluacion_Final_2025/solve_miercoles.py
@@ -0,0 +1,9 @@
+class ReglaTrapecio:
+    def __init__(self, funcion):
+        """
+        Inicializa la clase ReglaTrapecio con la función a integrar.
+
+        Args:
+            funcion: La función a integrar (debe aceptar un argumento numérico).
+        """
+        self.funcion = funcion
